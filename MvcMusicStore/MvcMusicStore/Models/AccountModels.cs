c0aee4648d70129c9a7d36ab749f4abd3c8edded
Nate B <nthomasbailey@gmail.com>
2014-02-11 18:28:17 -0500

    MvcMusicStore - Added administrator restriction to /Storemanager

--- MvcMusicStore/MvcMusicStore/Models/AccountModels.cs
+++ MvcMusicStore/MvcMusicStore/Models/AccountModels.cs
@@ -16,7 +16,7 @@ namespace MvcMusicStore.Models
         public string OldPassword { get; set; }
 
         [Required]
-        [StringLength(100, Errormessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "New password")]
         public string NewPassword { get; set; }
